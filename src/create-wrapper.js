f335a3af2cd261ced090518a0b96abffd333a898
xuegan <xuegan@didiglobal.com>
2022-12-01 00:01:38 +0800

    补充wrapper 应有的方法

--- src/create-wrapper.js
+++ src/create-wrapper.js
@@ -2,9 +2,88 @@
 class Wrapper {
   constructor(comp, options) {
     this.comp = comp
+    this.options = {}
   }
   attributes() {
     return this.comp.attributes
+  }
+  classes() {
+
+  }
+  contains() {
+
+  }
+  destroy() {
+
+  }
+  emitted() {
+
+  }
+  emittedByOrder() {
+
+  }
+  exists() {
+
+  }
+  find() {
+
+  }
+  findAll() {
+
+  }
+  findComponent() {
+
+  }
+  findAllComponent() {
+
+  }
+  html() {
+
+  }
+  get() {
+
+  }
+  is () {
+
+  }
+  isEmpty() {
+
+  }
+  isVisible() {
+
+  }
+  isVueInstance() {
+
+  }
+  name() {
+
+  }
+  props() {
+
+  }
+  setChecked() {
+
+  }
+  setData() {
+
+  }
+  setMethods() {
+
+  }
+  setProps() {
+
+  }
+  setSelected() {
+
+  }
+  setValue() {
+
+  }
+  text() {
+
+  }
+  trigger() {
+
   }
 }
 
